94c883c498c178ff9ad32e6a7ba6d2e33c8921ea
Lawton C. Mizell <alcamech@gmail.com>
2022-01-18 17:56:49 -0500

    updated pom.xml and module-info.java

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,7 +2,6 @@ module com.alcamech.jomo {
     requires javafx.controls;
     requires javafx.fxml;
     requires javafx.media;
-    requires javafx.graphics;
 
     opens com.alcamech.jomo to javafx.fxml;
     exports com.alcamech.jomo;
